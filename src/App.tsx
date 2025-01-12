1a8e59a99b69fd68631dbeb9fd4162506c7a5a9e
Amirull  Azmi <amirullazmi@happyhomes.id>
2025-01-12 23:43:58 +0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,42 @@
+
+import './App.css';
+import { useSyncProviders } from './useSyncProviders';
+
+const App = () => {
+	const providers = useSyncProviders();
+
+	const handleConnect = async (providerWithInfo: EIP6963ProviderDetail) => {
+		try {
+			const accounts = (await providerWithInfo.provider.request({
+				method: 'eth_requestAccounts',
+			})) as string[];
+		} catch (error) {
+			console.error(error);
+		}
+	};
+
+	return (
+		<div className='App'>
+			<h2>Wallets Detected:</h2>
+			<div className='providers'>
+				{providers.length > 0 ? (
+					providers?.map((provider: EIP6963ProviderDetail) => (
+						<button
+							key={provider.info.uuid}
+							onClick={() => handleConnect(provider)}>
+							<img
+								src={provider.info.icon}
+								alt={provider.info.name}
+							/>
+							<div>{provider.info.name}</div>
+						</button>
+					))
+				) : (
+					<div>No Announced Wallet Providers</div>
+				)}
+			</div>
+		</div>
+	);
+};
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,js,jsx,ts,tsx}",  // Add support for .ts, .tsx, .js, .jsx files
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
